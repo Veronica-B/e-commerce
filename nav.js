a6030abd3165d300883f0e8d1228a97b080e7a5e
Veronica Barcia <veronicabarcia4@gmail.com>
2020-03-30 15:44:16 -0400

    side nav is functional, not implmented on all pages

--- nav.js
+++ nav.js
@@ -1,23 +1,29 @@
-var hamburger_Icon= document.querySelector('.side_navigation_mobile');
-var side_nav_text= document.querySelector('.side_navigation_mobile__options');
-var i=0;
+var side_Nav= document.querySelector('.side_navigation_mobile');
+var x_icon= document.querySelector('.x');
+var hamburger_Icon= document.getElementById('side-nav');
 
 
-function show_Side_Nav(show){
-    hamburger_Icon.style.display= "block";
-  
+
+function show_Side_Nav(){
+    //will show the green side with side nav, and will add x and remove hamburger icon
+    side_Nav.style.display= "block";
+    x_icon.style.display="block";
+    hamburger_Icon.style.display="none"
 };
 
-function no_show_Side_Nav(no_show){
-    hamburger_Icon.style.display= "none";
-
+function no_show_Side_Nav(){
+    //the reverse of show_Side_Nav
+    side_Nav.style.display= "none";
+    x_icon.style.display="none";
+    hamburger_Icon.style.display="block"
 };
 
 
 
 
-    
-    document.getElementById('side-nav').addEventListener("click",show_Side_Nav);
+hamburger_Icon.addEventListener("click",show_Side_Nav);
+
+x_icon.addEventListener("click",no_show_Side_Nav);
 
 
 
